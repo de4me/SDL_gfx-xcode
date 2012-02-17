aa1e44c716b7f5169f1130e193e95aaa64020b7c
Andreas Schiffler <827652+ferzkopp@users.noreply.github.com>
2012-02-17 04:10:21 +0000

    Remove cruft file; update copyright year

--- SDL_gfxPrimitives.c
+++ SDL_gfxPrimitives.c
@@ -2,7 +2,7 @@
 
 SDL_gfxPrimitives.c: graphics primitives for SDL surfaces
 
-Copyright (C) 2001-2011  Andreas Schiffler
+Copyright (C) 2001-2012  Andreas Schiffler
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
