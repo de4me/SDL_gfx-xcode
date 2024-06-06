a854fbea27df67aaa33ab344b483b850340b1dec
Denis Popov <aoh@list.ru>
2024-06-06 16:08:47 +0300

    Added Xcode project;

---
+++ Xcode/SDL_gfx/SDL_gfx/SDL_gfx.h
@@ -0,0 +1,23 @@
+//
+//  SDL_gfx.h
+//  SDL_gfx
+//
+//  Created by DE4ME on 28.09.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDL_gfx.
+FOUNDATION_EXPORT double SDL_gfxVersionNumber;
+
+//! Project version string for SDL_gfx.
+FOUNDATION_EXPORT const unsigned char SDL_gfxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDL_gfx/PublicHeader.h>
+
+#import <SDL_gfx/SDL_framerate.h>
+#import <SDL_gfx/SDL_gfxBlitFunc.h>
+#import <SDL_gfx/SDL_gfxPrimitives_font.h>
+#import <SDL_gfx/SDL_gfxPrimitives.h>
+#import <SDL_gfx/SDL_imageFilter.h>
+#import <SDL_gfx/SDL_rotozoom.h>
